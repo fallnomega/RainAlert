4defcc24bfcecdea104b4da7c69230250a54925d
fallnomega <fallnomega@gmail.com>
2023-02-28 18:21:07 -0800

    stored keys/tokens/etc in env

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ import datetime
 import twilio.rest
 from pytz import timezone
 from twilio.rest import Client
+import os
 
-API_KEY = "YOUR OPEN WEATHER MAP API KEY"
-ACCOUNT_SID = 'YOUR TWILIO ACCOUNT SID'
-AUTH_TOKEN = 'YOUR TWILIO AUTH TOKEN '
+# STORE KEYS IN ENV
+API_KEY = os.environ.get("OWM_API")  # OPEN WEATHER APP API KEY
+ACCOUNT_SID = os.environ.get("ACCOUNT_SID")  # TWILIO ACCOUNT SID
+AUTH_TOKEN = os.environ.get("AUTH_TOKEN")  # TWILIO AUTH TOKEN
 
 # example with lat and lon -
 # https: // api.openweathermap.org / data / 2.5 / forecast?lat = 37.931961 & lon = -121.695930 & appid = 1176e9ae092df65480e70f96400cb3a6
@@ -31,6 +33,7 @@ def get_weather():
     data = requesting.json()
     return data
 
+
 def get_umbrell(weather_report):
     weather_slice = weather_report["hourly"][:11]
     umbrella = []
@@ -40,10 +43,12 @@ def get_umbrell(weather_report):
             local_time = timezone('US/Pacific').localize(local_time)
             umbrella.append(f"Date: {local_time.strftime('%m/%d/%Y %H:%M:%S %Z')} - Bring An Umbrella !")
             my_twillio = twilio.rest.Client(ACCOUNT_SID, AUTH_TOKEN)
-            message = my_twillio.messages.create(body=f"Date: {local_time.strftime('%m/%d/%Y %H:%M:%S %Z')} - Bring An Umbrella !",
-                                                 from_='TWILIO PHONE NUMBER OR SOMETHING ELSE',
-                                                 to='PHONE NUMBER TO SEND IT TO')
+            message = my_twillio.messages.create(
+                body=f"Date: {local_time.strftime('%m/%d/%Y %H:%M:%S %Z')} - Bring An Umbrella !",
+                from_='TWILIO PHONE NUMBER OR SOMETHING ELSE',
+                to='PHONE NUMBER TO SEND IT TO')
             print(message.status)
+
 
 my_weather = get_weather()
 get_umbrell(my_weather)
